39bee5949561995cb06b99b14d8e643c8072e773
Diego Zapala <diego.zapala@gmail.com>
2017-01-18 13:10:03 -0200

    Ajustando configuracoes para o redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 # set redis connection
-uri = URI.parse(ENV['REDIS_URL'])
+uri = URI.parse(ENV['REDIS_URL'] || "redis://localhost:6379/" )
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
