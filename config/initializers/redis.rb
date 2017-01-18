eabf0c46c8a2787eab4c2330cb1ebc01f92cedd0
Diego Zapala <diego.zapala@gmail.com>
2017-01-18 13:15:52 -0200

    Ajustando configuracoes para o redis em producao

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
 # set redis connection
-uri = URI.parse(ENV['REDIS_URL'] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(url: ENV["REDIS_URL"])
