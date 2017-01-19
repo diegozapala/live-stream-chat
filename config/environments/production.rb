ceed8ea60edbe3119c528afec3b03561b3dfe477
Diego Zapala <diego.zapala@gmail.com>
2017-01-18 23:39:20 -0200

    Ajustando configuracoes para o action cable em producao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,13 +17,15 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
+  config.web_socket_server_url = "wss://action-cable-example.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-example.herokuapp.com', 'http://action-cable-example.herokuapp.com']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
